4c4dcbf965010e641a0976a4262731de1ffd890b
Lukas Schauer <lukas@schauer.so>
2014-03-03 02:47:46 +0100

    Sync-Script ohne absolute Pfadangabe

--- sync.sh
+++ sync.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 ssh root@10.8.0.3 mount -o remount,rw /
 
 rsync \
     --archive \
-    ~/Projects/Bat-Projects/Lichtschranke/ \
+    ${DIR}/ \
     -e ssh \
     root@10.8.0.3:/home/root/lichtschranke/ \
     --progress \
     --exclude '*.dtbo' \
+    --exclude '*.pyc' \
+    --exclude '*.pyo' \
+    --exclude '*.pyd' \
     --exclude '.git' \
     --delete
 
