979dac3634203a87873741c0a88b56a9a940732f
Lukas Schauer <lukas@schauer.so>
2014-03-11 16:25:52 +0100

    Beim Abholen von SMS auch gleich mal die aktuelle Signalstärke mitloggen, wahrscheinlich uninteressant, aber naja, man kanns halt...

--- sms/sms.sh
+++ sms/sms.sh
@@ -2,5 +2,6 @@
 sleep 120 # 120 minuten warten, so dass alles andere schon läuft
 while true; do
     gnokii --config /home/root/lichtschranke/sms/gnokiirc --getsms SM 0 end -a /home/root/logs/sms.txt -d 2> /dev/null
+    (echo -n $(date +%s)': '; (chat -V -s '' 'AT+CSQ' 'OK' '' > /dev/ttyUSB1 < /dev/ttyUSB1) 2>&1 | grep ^+CSQ | awk '{print $2}') >> /home/root/logs/csq.txt
     sleep 600
 done
