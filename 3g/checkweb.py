b6e2a34676e3f979c00f0894370c36dbd4d2c30f
Lukas Schauer <lukas@schauer.so>
2014-03-27 02:02:27 +0100

    Nach 2 Tagen ohne Internetverbindung einen Reboot versuchen (in eingeschränkten Zeitrahmen)

--- 3g/checkweb.py
+++ 3g/checkweb.py
@@ -1,13 +1,17 @@
 import urllib2
 import os
 import time
+import datetime
 
 delay = 180 # seconds => 3 minutes
 max_fails = 4 # * 3 minutes => 12 minutes
+max_hard_fails = 960 # * 3 minutes => 2 days
+
 check_url = "http://pass.telekom.de"
 second_check_url = "http://google.de" # just in case pass.telekom.de is down...
 
 cur_fails = 0
+cur_hard_fails = 0
 
 def internet_on():
     global check_url
@@ -30,13 +34,22 @@ def reconnect():
     except:
         pass
 
+def rebootmaybe():
+    curhour = datetime.datetime.now().hour
+    if curhour > 8 and curhour < 17:
+        os.system("reboot")
+
 print("I'M GONNA CHECK FOR DA WEB!")
 
 while True:
     if internet_on():
         cur_fails = 0
+        cur_hard_fails = 0
     else:
-        cur_fails = cur_fails + 1
+        cur_fails += 1
+        cur_hard_fails += 1
         if cur_fails > max_fails:
             reconnect()
+        if cur_hard_fails > max_hard_fails:
+            rebootmaybe()
     time.sleep(delay)
